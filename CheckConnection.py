4da087516a4f7731ad1d733cbc58d618ca0452b9
Azhar Khan <azhar.khan.78601@gmail.com>
2016-02-07 13:03:44 +0530

    Update CheckConnection.py

--- CheckConnection.py
+++ CheckConnection.py
@@ -1,6 +1,6 @@
 import pymysql
 try:
-    con=pymysql.connect(host='localhost',port=3306,user='root',passwd='root',db='sample')
+    con=pymysql.connect(host='localhost',port=3306,user='root',passwd='root',db='mysql')
     print("Connection Successful :-)")
 except:
     print("Connection Unsuccessful :-(")
